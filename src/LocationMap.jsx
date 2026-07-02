0f3faeb87f7427df8bd79b0635eb7c055b34ce3a
rizkyilhampra <rizkyilhamp16@gmail.com>
2026-07-02 15:51:15 +0800

    style(LocationMap): reduce map height for better layout consistency

--- src/LocationMap.jsx
+++ src/LocationMap.jsx
@@ -5,7 +5,7 @@ const BANJARMASIN = [114.590111, -3.316694];
 
 export default function LocationMap() {
   return (
-    <div className="relative mt-6 h-64 w-full overflow-hidden rounded-lg border border-border bg-card/50 sm:h-80 md:w-1/2">
+    <div className="relative mt-6 h-40 w-full overflow-hidden rounded-lg border border-border bg-card/50 sm:h-44 md:w-1/2">
       <Map center={KALSEL_CENTER} zoom={6.5}>
         <MapMarker longitude={BANJARMASIN[0]} latitude={BANJARMASIN[1]}>
           <MarkerContent />
